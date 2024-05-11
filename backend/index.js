66ad21a958ad2fc5851190937e4a2445ce19977a
Egorlaptop <egorlavrinovich@gmail.com>
2024-05-11 18:17:31 +0300

    update enviroment variables

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const router = require("./router.js");
 const cors = require("cors");
 
-const PORT = 5000;
-const DB_URL =
-  "mongodb://egorlavrinovich:user@ac-1crtasn-shard-00-00.qdjkfij.mongodb.net:27017,ac-1crtasn-shard-00-01.qdjkfij.mongodb.net:27017,ac-1crtasn-shard-00-02.qdjkfij.mongodb.net:27017/?ssl=true&replicaSet=atlas-ydpl7y-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
 
 const app = express();
 app.use(cors());
@@ -21,6 +20,8 @@ const startApp = async () => {
     console.log(error);
   }
 };
+
+console.log(DB_URL);
 
 startApp();
 
